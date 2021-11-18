3a64927111d20e09d8d976d4a5df02e4f7e5be00
MrPig91 <sirius.cleveland@gmail.com>
2021-11-17 23:02:35 -0600

    Added Public Function Get-slBattleHistory

---
+++ PSSplinterlands/Public/Get-slBattleHistory.ps1
@@ -0,0 +1,32 @@
+function Get-slBattleHistory {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory,ValueFromPipeline,ValueFromPipelineByPropertyName)]
+        [Alias("UserName")]
+        [string]$PlayerName
+    )
+
+    try{
+        $Battles = Invoke-SplinterlandsAPI -Uri "https://game-api.splinterlands.io/battle/history?player=$PlayerName"
+        foreach ($battle in $Battles.battles){
+            $battle.details = $battle.details | ConvertFrom-Json
+            $Teams = @($battle.details.team1,$battle.details.team2)
+            if ($PlayerName -eq $battle.player_1){
+                $Enemy = $battle.player_2
+            }
+            else {
+                $Enemy = $battle.player_1
+            }
+            $battle | Add-Member -NotePropertyMembers @{
+                Player = $PlayerName
+                Enemty = $Enemy
+                PlayerTeam = $Teams | where player -eq $PlayerName
+                EnemyTeam = $Teams | where player -eq $Enemy
+            }
+            $battle
+        }
+    }
+    catch{
+        $PSCmdlet.WriteError($_)
+    }
+}
